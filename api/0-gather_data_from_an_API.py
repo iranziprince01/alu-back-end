77da7c0ecac3bb118b1f76e738d0cfd6f6ed6f13
iranziprince01 <p.iranzi@alustudent.com>
2023-07-09 08:41:26 +0000

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
-#!/usr/bin/python3
-"""
-Script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress
-"""
+!/usr/bin/python3
+# Script that, using this REST API, returns information about his/her TODO list
 
 import json
 import requests
